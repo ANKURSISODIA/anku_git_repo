9ff1500f22f685461b70f41edf820c0fa9d22634
ANKURSISODIA <ankishu@gmail.com>
2019-09-26 15:46:45 +0530

    kycproject_openshift

---
+++ src/test/java/com/boa/kycprocess/KycprocessApplicationTests.java
@@ -0,0 +1,18 @@
+package com.boa.kycprocess;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KycprocessApplicationTests {
+
+	@Ignore
+	@Test
+	public void contextLoads() {
+	}
+
+}
